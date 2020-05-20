e1e11c3bb3d7e49a2b6a83c0ecfc750e8880f109
Avishka1998 <pavinduavishka@gmail.com>
2020-05-20 20:46:19 +0530

    changed the order

--- Assignment1.scala
+++ Assignment1.scala
@@ -2,15 +2,15 @@ object Assignment1{
 
 def main(args: Array[String]) {
 
-//1
+//Q.1
 def C(x:Double)=x*1.8+32.0;
 println(C(35.0));
 
-//2
+//Q.2
 def volume(r:Double) = 4.0/3.0*3.14*r*r*r;
 println(volume(5.0));
 
-//3
+//Q.3
 def books(n:Int):Double = n*24.95;
       def discount(a:Double):Double = a*0.4;
       def shipping(n:Int):Double = {
