401a8c22ab1eddb8b45eaf0152fee55eeca2281f
Avishka1998 <56598928+Avishka1998@users.noreply.github.com>
2020-05-30 16:16:16 +0530

    Create primenumber.scala

---
+++ Assignment3/primenumber.scala
@@ -0,0 +1,30 @@
+object primenumber{
+	
+	def prime(a:Int,b:Int=2):Boolean = {
+		if(a==2){
+			return true;
+			}
+
+		else if(a<2){
+			return false;
+			}
+
+		else if(a%b==0){
+			return false;;
+			}
+
+		else if(b*b>a){
+			return true;
+			}
+
+		else{
+			return prime(a,b+1);
+			}
+		}	
+
+	def main(arg: Array[String]){
+		println(prime(5));
+		println(prime(8));
+		}
+
+}
