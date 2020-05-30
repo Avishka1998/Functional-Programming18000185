41685e1cfa2610385fdebdcc404dfb342208c8f0
Avishka1998 <56598928+Avishka1998@users.noreply.github.com>
2020-05-30 16:29:54 +0530

    Addition of less than n

---
+++ Assignment3/addition.scala
@@ -0,0 +1,18 @@
+object addition{
+	def addition(n:Int):Int = {
+		if(n==0){
+			return 0;
+			}
+
+		else{
+			var res:Int = n+addition(n-1);
+			return res;
+			}
+		}
+
+	def main(args: Array[String]){
+
+		println(addition(5));	
+
+		}
+}
